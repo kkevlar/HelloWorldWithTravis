dfcad8bda30b76b3b397a7f5e52e200aef6177c4
Kevin Kellar <kellar.kevin@gmail.com>
2019-01-25 15:17:48 -0800

    exclaim

--- src/helloworld/HelloWorldMain.java
+++ src/helloworld/HelloWorldMain.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorldMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World - lmao");
+		System.out.println("Hello World lmao!");
 
 	}
 
